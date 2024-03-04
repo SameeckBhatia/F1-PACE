ea1d9ba3097e4bb1aad340d58386e1698540f17e
SameeckBhatia <canadianmg@outlook.com>
2024-03-04 10:00:58 -0500

    Assigning colors to constructors

--- report-script.R
+++ report-script.R
@@ -5,3 +5,10 @@ library(tidyverse)
 library(tidyquant)
 
 lap_times <- read.csv("python/lap_times.csv")
+
+constructor_colors <- list(
+  "Alpine" = "#FF87BC", "Aston Martin" = "#229971", "Ferrari" = "#E8002D",
+  "Haas" = "#B6BABD", "Kick Sauber" = "#52E252", "McLaren" = "#FF8000",
+  "Mercedes" = "#27F4D2", "Racing Bulls" = "#6692FF", "Red Bull" = "#3671C6",
+  "Williams" = "#64C4FF"
+)
