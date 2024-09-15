f681c9fd8fed109eda08fd589cb9f798cbe794b3
SameeckBhatia <94806847+SameeckBhatia@users.noreply.github.com>
2024-09-15 18:43:02 -0400

    Adding Singapore as round 18

--- main_script.py
+++ main_script.py
@@ -14,12 +14,12 @@ from bs4 import BeautifulSoup
 
 # Creating a data frame for the rounds, Grands Prix, and number of laps
 grand_prix_df = pd.DataFrame({
-    "round_num": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    "round_num": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
     "grand_prix": ["Bahrain", "Saudi Arabia", "Australia", "Japan", "China",
                    "Miami", "Emilia Romagna", "Monaco", "Canada", "Spain",
                    "Austria", "Great Britain", "Hungary", "Belgium",
-                   "Netherlands", "Italy", "Azerbaijan"],
-    "laps": [57, 50, 58, 53, 56, 57, 63, 78, 70, 66, 71, 52, 70, 44, 72, 53, 51]
+                   "Netherlands", "Italy", "Azerbaijan", "Singapore"],
+    "laps": [57, 50, 58, 53, 56, 57, 63, 78, 70, 66, 71, 52, 70, 44, 72, 53, 51, 62]
 })
 
 # Setting the round number as the data frame index
